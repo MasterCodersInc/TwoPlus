21d0cc162bc53e81b1012034e60aadb09019e057
joetoenails <joe.d.tonelli@gmail.com>
2021-02-02 21:33:35 -0500

    added lite styling and postType to addPost page

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,34 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import EditorUID from './EditorUID'
-import {useAuth} from '../contexts/AuthContext'
-import ChatRoom from './ChatRoom'
-import firebase from '../firebase'
+import React, { useEffect, useState } from "react";
+import EditorUID from "./EditorUID";
+import { useAuth } from "../contexts/AuthContext";
+import ChatRoom from "./ChatRoom";
+import firebase from "../firebase";
 
 const Post = (props) => {
-    const {currentUser} = useAuth();
-    const [post, setPost] = useState('');
-    let title;
-    //grab post from DB
-    const postRef = firebase.firestore().collection('posts').doc(`${props.match.params.postId}`)
+  const { currentUser } = useAuth();
+  const [post, setPost] = useState("");
+  let title;
+  //grab post from DB
+  const postRef = firebase
+    .firestore()
+    .collection("posts")
+    .doc(`${props.match.params.postId}`);
 
-    useEffect(() => {
-       async function getPostData(){
-            const postFromDb = await postRef.get();
-            const postData = postFromDb.data();
-            setPost(postData);
-        }
-        getPostData();
-    },[])
+  useEffect(() => {
+    async function getPostData() {
+      const postFromDb = await postRef.get();
+      const postData = postFromDb.data();
+      setPost(postData);
+    }
+    getPostData();
+  }, []);
 
-    title = post ? post.title : '';
-    return (
-        <div>
-            <div>{post.title || ''}</div>
-            <div>{post.description || ''}</div>
-            <EditorUID uid={currentUser.uid}/>
-            <ChatRoom />
-        </div>
-    )
-}
+  //if typeOf post === "discuss", return Discuss Comp
+
+  //if typeOf post === "you", return You Comp
+
+  //if typeOf post === "live", return Live Editor
+
+  return (
+    <div>
+      <div>{post.title || ""}</div>
+      <div>{post.description || ""}</div>
+      <EditorUID uid={currentUser.uid} />
+      <ChatRoom />
+    </div>
+  );
+};
 
 export default Post;
