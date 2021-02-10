c7b937998f027cfdd44736fd9b9cec36c0b8bf18
joetoenails <joe.d.tonelli@gmail.com>
2021-02-10 02:07:30 -0500

    light styling to discuss

--- src/components/UserLanding.js
+++ src/components/UserLanding.js
@@ -247,15 +247,16 @@ export default function Landing() {
                       <Grid
                         item
                         style={{ marginTop: ".5em", marginLeft: "1em" }}
-                      >
-                        <Typography variant="body2">#hashtags</Typography>
-                      </Grid>
+                      ></Grid>
                     </Grid>
 
                     <Grid item container direction="row">
                       {post.tags.map((tag) => {
                         return (
                           <Typography
+                            component={Link}
+                            to={`/posts/?tag=${tag}`}
+                            className={classes.postLink}
                             variant="body2"
                             style={{
                               color: "white",
